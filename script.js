55894a6a463aa532bdb0e34d43ca2d647627488f
Oseni Adedoyin <oseniadedoyin22@gmail.com>
2021-09-02 07:01:05 -0700

    added fuctionalty

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 $(document).ready(function () {
+  //to change nav(active) on scroll
+  $(window)
+    .scroll(function () {
+      let scrollDistance = $(window).scrollTop() - -400;
+
+      $(".anchor").each(function (i) {
+        if ($(this).position().top <= scrollDistance) {
+          $(".headerNav a.active").removeClass("active");
+          $(".headerNav a").eq(i).addClass("active");
+        }
+      });
+    })
+    .scroll();
+
+  //to show headerNav for small screen on menubar toggle
   $(".menuIcon").click(function () {
     $(".headerNav").toggleClass("addHeaderNav");
+  });
+
+  // to toggle active class on nav click and also close headernav for small screen
+
+  $(".headerNav a").click(function () {
+    $(".headerNav a").removeClass("active");
+    $(this).addClass("active");
+    $(".headerNav").removeClass("addHeaderNav");
+  });
+
+  //to select portfolio types based on type selected
+  $(".portfolioLists li").click(function (e) {
+    if (e.target.id == "javascript") {
+      $(".portfolioProject").hide();
+      $(".javascript").show("slow");
+    }
+
+    if (e.target.id == "react") {
+      $(".portfolioProject").hide();
+      $(".react").show("slow");
+    }
+
+    if (e.target.id == "all") {
+      $(".portfolioProject").show("slow");
+    }
+
+    // if (e.target.id == "next") {
+    //   $(".portfolioProject").hide();
+    //   $(".next").show();
+    // }
   });
 });
